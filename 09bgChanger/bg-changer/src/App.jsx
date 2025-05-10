7ca745afa0dccd1cca5a380716aed1cae01c7a3a
Sandipan Deb <sandipandebgithub@gmail.com>
2025-05-10 14:39:22 +0530

    Project Done

--- 09bgChanger/bg-changer/src/App.jsx
+++ 09bgChanger/bg-changer/src/App.jsx
@@ -1,10 +1,40 @@
-
+import React from 'react'
+import { useState } from 'react'
 
 function App() {
-
+  const [bgColor, setBgColor] = useState("white");
   return (
     <>
-      <h1>Background Color Changer</h1>
+      <div className="w-full h-screen duration-200" style={{backgroundColor : bgColor}}></div>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button 
+            className='flex justify-center items-center px-4 py-1 rounded-full shadow-lg outline-none'
+            style={{backgroundColor : "crimson", color : 'white'}}
+            onClick={() => setBgColor("crimson")}
+          >Red</button>
+          <button 
+            className='flex justify-center items-center px-4 py-1 rounded-full shadow-lg outline-none'
+            style={{backgroundColor : "cyan", color : 'blue'}}
+            onClick={() => setBgColor("cyan")}
+          >Cyan</button>
+          <button 
+            className='flex justify-center items-center px-4 py-1 rounded-full shadow-lg outline-none'
+            style={{backgroundColor : "Green", color : 'white'}}
+            onClick={() => setBgColor("green")}
+          >Green</button>
+          <button 
+            className='flex justify-center items-center px-4 py-1 rounded-full shadow-lg outline-none'
+            style={{backgroundColor : "blue", color : 'white'}}
+            onClick={() => setBgColor("blue")}
+          >Blue</button>
+          <button 
+            className='flex justify-center items-center px-4 py-1 rounded-full shadow-lg outline-none'
+            style={{backgroundColor : "white", color : 'black'}}
+            onClick={() => setBgColor("#fff")}
+          >White</button>
+        </div>
+      </div>
     </>
   )
 }
